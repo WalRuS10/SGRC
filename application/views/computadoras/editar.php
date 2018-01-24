37c751a2bd9861333f48f21ac127c4ebd056ac4d
Diego Arnaudin <d.aspirina@gmail.com>
2018-01-24 19:22:16 -0300

    Computadoras

---
+++ application/views/computadoras/editar.php
@@ -0,0 +1,34 @@
+<div class="row">
+    <div class="col-md-12">
+      	<div class="box box-info">
+            <div class="box-header with-border">
+              	<h3 class="box-title">Editar Computadora</h3>
+            </div>
+			<?php echo form_open('computadoras/editar/'.$computadora['ID_COMPUTADORA']); ?>
+					<div class="form-group">
+						<label for="CLIENTE" class="col-md-4 control-label">Cliente</label>
+						<div class="col-md-8">
+							<select name="CUIT_CLIENTE" size=1>
+							<? foreach($lista_clientes as $cli): ?>
+							<option value=<?=$cli['CUIT'];?>><?=$cli['RAZON_SOCIAL'];?></option>
+							<? endforeach; ?>
+							</select>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="FECHA_INGRESO" class="col-md-4 control-label">Fecha de Ingreso</label>
+						<div class="col-md-8">
+							<input type="text" name="FECHA_INGRESO" value="<?php echo ($this->input->post('FECHA_INGRESO') ? $this->input->post('FECHA_INGRESO') : $computadora['FECHA_INGRESO']); ?>" class="form-control" id="FECHA_INGRESO" />
+						</div>
+					</div>
+
+			<div class="form-group">
+            	<button type="submit" class="btn btn-success">
+					<i class="fa fa-check"></i> Guardar
+				</button>
+				<a class="btn btn-danger" href="<?=site_url('computadoras')?>" >Cancelar</a>
+	        </div>				
+			<?php echo form_close(); ?>
+		</div>
+    </div>
+</div>
---
+++ application/views/computadoras/index.php
@@ -0,0 +1,33 @@
+<h1>Computadoras</h1>
+<?=form_open('computadoras/buscar');?>
+<div class="form-group">
+	<?=form_label('ID de Computadora: ', 'id_computadora');?>
+	<?=form_input('id_computadora', '0');?>
+	<?=form_submit('buscar','Buscar');?>
+</div>
+<?=form_close();?>
+<div class="pull-right">
+	<a href="<?php echo site_url('computadoras/nuevo'); ?>" class="btn btn-success">Nuevo</a> 
+</div>
+<table class="table table-light">
+
+<tr>
+	<th>ID</th>
+	<th>Fecha de Ingreso</th>
+	<th>Cliente</th>
+	<th>Acciones</th>
+</tr> 
+
+<? foreach($lista_computadoras as $com): ?>
+	<tr>
+		<td><?=$com['ID_COMPUTADORA']?></td>
+		<td><?=$com['FECHA_INGRESO']?></td>
+		<td><?=$com['CUIT_CLIENTE']?></td>
+		<td>
+			<a class="btn btn-success" href="<?=site_url('computadoras/editar/'.$com['ID_COMPUTADORA'])?>" >Editar</a>
+			<a class="btn btn-danger" href="<?=site_url('computadoras/eliminar/'.$com['ID_COMPUTADORA'])?>" >Eliminar</a>
+		</td>
+	</tr>
+<? endforeach; ?>
+
+</table>
